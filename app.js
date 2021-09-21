5ffb072445ac5ad95f654abb261119bec4d984d6
rutu14 <vidyapunjabi7@gmail.com>
2021-09-21 20:07:28 +0530

    changed the wrong api url

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate=document.querySelector("#btn-translate")
 var txtarea=document.querySelector("#txt")
 var outtxt=document.querySelector("#outtxt")
 
-url_server="https://api.funtranslations.com/translate/minion.json"
+url_server="https://api.funtranslations.com/translate/ferb-latin.json"
 function gettranslation(input){
     return url_server+"?"+"text="+input
 }
@@ -11,8 +11,7 @@ btnTranslate.addEventListener("click",function clickEventHandler(){
     var input=txtarea.value
     fetch(gettranslation(input))
         .then(response => response.json())
-        .then(json=>{
-            outtxt.innerText=json.contents.translated})
+        .then(json=>{outtxt.innerText=json.contents.translated})
         .catch(function errorHandler(error){
             console.log("Server Error.")
             alert("Server error!")
